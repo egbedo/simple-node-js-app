8bc447bb387f85c3dab46703d7b64e2bbf9e2ebf
egbedo <egbedodaniel@gmail.com>
2024-08-29 06:53:42 -0700

    added files

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const requestHandler = (req, res) => {
+  res.end('Hello, World!');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
